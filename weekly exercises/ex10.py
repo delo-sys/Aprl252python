d9e6d68a9233eacbec3beb7f10efa35583efd4c5
delo-sys <mrodhiambo226@gmail.com>
2025-04-14 14:54:34 +0300

    fourth commit

---
+++ weekly exercises/ex10.py
@@ -0,0 +1,7 @@
+day=float(input("enter day"))
+month=float(input("enter month in numeric form"))
+year=float(input("enter year in two digit"))
+if day*month==year:
+    print("magic date")
+elif day*month==year:
+    print("not a magic date")
